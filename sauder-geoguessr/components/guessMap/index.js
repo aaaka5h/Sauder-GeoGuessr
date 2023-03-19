2393cc0d14df5561687028f22c79389177b730e7
aaaka5h <akash7adhikary@gmail.com>
2023-03-18 18:32:16 -0700

    Styled buttons

--- sauder-geoguessr/components/guessMap/index.js
+++ sauder-geoguessr/components/guessMap/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import FloorGuess from "../floorGuess";
+import styles from "./GuessMap.module.css";
 
 function GuessMap({ setGuess }) {
   const [floor, setFloor] = useState(null);
@@ -7,21 +8,28 @@ function GuessMap({ setGuess }) {
   return (
     <div>
       <div>
-        <button onClick={() => setFloor(0)}>Floor B</button>
-        <button onClick={() => setFloor(1)}>Floor 1</button>
-        <button onClick={() => setFloor(2)}>Floor 2</button>
-        <button onClick={() => setFloor(3)}>Floor 3</button>
-        <button onClick={() => setFloor(4)}>Floor 4</button>
-        <button onClick={() => setFloor(9)}>Floor 9</button>{" "}
+        <button className={styles.floorButton} onClick={() => setFloor(0)}>
+          Floor B
+        </button>
+        <button className={styles.floorButton} onClick={() => setFloor(1)}>
+          Floor 1
+        </button>
+        <button className={styles.floorButton} onClick={() => setFloor(2)}>
+          Floor 2
+        </button>
+        <button className={styles.floorButton} onClick={() => setFloor(3)}>
+          Floor 3
+        </button>
+        <button className={styles.floorButton} onClick={() => setFloor(4)}>
+          Floor 4
+        </button>
+        <button className={styles.floorButton} onClick={() => setFloor(9)}>
+          Floor 9
+        </button>{" "}
         {/* floor 9 is treated as floor 5*/}
       </div>
 
-      {floor != null ? (
-        <FloorGuess
-          setGuess={setGuess}
-          floor={floor}
-        />
-      ) : null}
+      {floor != null ? <FloorGuess setGuess={setGuess} floor={floor} /> : null}
     </div>
   );
 }
