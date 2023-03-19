cb77e553572ec7c6e17dc76009b236b249f51f57
aaaka5h <akash7adhikary@gmail.com>
2023-03-18 19:17:50 -0700

    Latest results styling

--- sauder-geoguessr/components/resultContainer/index.js
+++ sauder-geoguessr/components/resultContainer/index.js
@@ -4,24 +4,25 @@ import styles from "../../styles/Home.module.css";
 
 const ResultContainer = ({ guess, answer }) => {
   const [guessMarker, setGuessMarker] = useState(null);
-  const [answerMarker, setAnswerMarker] = useState(null) 
+  const [answerMarker, setAnswerMarker] = useState(null);
 
   useEffect(() => {
     setGuessMarker(null);
     setAnswerMarker(null);
   });
 
-  const text = () => 
-    {
-        return guess.floor === answer.floor ? "" : "You guessed the wrong floor, 250 points deducted"
-    }
+  const text = () => {
+    return guess.floor === answer.floor
+      ? ""
+      : "You guessed the wrong floor! Correct floor was: Floor " + answer.floor;
+  };
 
   const getFloorPath = () => {
     console.log(`/../public/images/Level_${answer.floor}.png`);
     return `/../public/images/Level_${answer.floor}.png`;
   };
 
-  console.log(answer, guess)
+  console.log(answer, guess);
 
   return (
     <div style={{ position: "relative", height: "550px" }}>
@@ -30,34 +31,37 @@ const ResultContainer = ({ guess, answer }) => {
         alt="Floorplan"
         width={720}
         height={450}
-        style={{ maxWidth: "100%" }}
+        style={{ maxWidth: "100%", border: "4px solid black", padding: "10px" }}
       />
-      
-    <div
+
+      <div
         style={{
-        position: "absolute",
-        left: guess.x - 10,
-        top: guess.y - 32,
+          position: "absolute",
+          left: guess.x - 10,
+          top: guess.y - 32,
         }}
-    >
+      >
         <Image src="/images/marker.png" alt="Marker" width={20} height={30} />
-    </div>
-
-        
-    <div
-        style={{
-        position: "absolute",
-        left: answer.x - 10,
-        top: answer.y - 32,
-        }}
-    >
-        <Image src="/images/answermarker.png" alt="Marker" width={20} height={30} />
-    </div>
-
-    <div style={{ textAlign: 'center', marginTop: '10px' }} >
-        <p className={styles.description}>{text()}</p>
       </div>
 
+      <div
+        style={{
+          position: "absolute",
+          left: answer.x - 10,
+          top: answer.y - 32,
+        }}
+      >
+        <Image
+          src="/images/answermarker.png"
+          alt="Marker"
+          width={20}
+          height={30}
+        />
+      </div>
+
+      <div style={{ textAlign: "center", marginTop: "10px" }}>
+        <p className={styles.description}>{text()}</p>
+      </div>
     </div>
   );
 };
